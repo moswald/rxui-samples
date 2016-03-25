707f70376d9fc7ebb9abf082585c0732f8e4e2c0
Aszan <aszan06@gmail.com>
2016-03-25 23:51:28 +0100

    Design time data friendly. Switch x:Bind to Binding to display

--- ReactiveReader/ReactiveReader.Core/ViewModels/DesignTime/DesignTimeFeedsViewModel.cs
+++ ReactiveReader/ReactiveReader.Core/ViewModels/DesignTime/DesignTimeFeedsViewModel.cs
@@ -1,10 +1,5 @@
 ﻿using ReactiveUI;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Reactive;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ReactiveReader.Core.ViewModels.DesignTime
 {
@@ -12,18 +7,19 @@ namespace ReactiveReader.Core.ViewModels.DesignTime
     {
         public DesignTimeFeedsViewModel()
         {
-            Blogs = new ReactiveList<BlogViewModel>()
+            Blogs = new ReactiveList<IBlogViewModel>()
             {
-                new BlogViewModel("John's Blog", new Uri("http://example.com/feed.rss")),
-                new BlogViewModel("Jill's Blog", new Uri("http://example.com/feed.rss"))
+                new DesignTimeBlogViewModel(),
+                new DesignTimeBlogViewModel()
             };
         }
 
         public ReactiveCommand<Unit> RemoveBlog { get; }
         public ReactiveCommand<Unit> AddBlog { get; }
         public ReactiveCommand<Unit> PersistData { get; }
-        public ReactiveList<BlogViewModel> Blogs { get; set; }
+        public ReactiveList<IBlogViewModel> Blogs { get; set; }
         public ReactiveCommand<Unit> RefreshAll { get; }
         public bool IsLoading { get; }
+        public IBlogViewModel SelectedBlog { get; }
     }
 }
